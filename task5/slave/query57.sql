f8c5d519ab96f25c6ea1b69119437734ec426e2b
mikhailyesman <43871862+mikhailyesman@users.noreply.github.com>
2021-02-15 19:21:21 +0300

    Delete query57.sql

--- task5/slave/query57.sql
+++
@@ -1,9 +0,0 @@
-mysql -u smes -p <<EOF
-USE db_group
-  SELECT Student
-    FROM Students
-      WHERE StudentID IN (
-      SELECT StudentID
-      FROM Students
-    WHERE Student="${USERNAME}")
-EOF
